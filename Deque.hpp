4ddb1f87529df8c3845fb72a2534efd2cb201dec
Jesse Struck <jdstruck@gmail.com>
2020-09-19 18:38:49 -0400

    continue work on Iterator at assert at test.cpp:190

--- Deque.hpp
+++ Deque.hpp
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
-
+#define CAP 5
 #define Deque_DEFINE(T)                                                        \
     /*typedef T* Deque_##T##_Iterator;*/                                       \
     struct Deque_##T##_Iterator {                                              \
         T *data_ptr;                                                           \
         int curr_idx;                                                          \
-        int capacity;                                                          \
+        int deque_capacity;                                                    \
         void (*inc)(Deque_##T##_Iterator*);                                    \
         void (*dec)(Deque_##T##_Iterator*);                                    \
         T &(*deref)(Deque_##T##_Iterator*);                                    \
     };                                                                         \
     struct Deque_##T {                                                         \
-        T data[10];                                                            \
+        T data[CAP];                                                            \
         size_t capacity;                                                       \
         size_t curr_size;                                                      \
         int f_idx;                                                             \
@@ -33,7 +33,8 @@
         size_t (*size)(Deque_##T *);                                           \
         bool (*empty)(Deque_##T *);                                            \
         bool (*full)(Deque_##T *);                                             \
-        void (*delet)(Deque_##T *);                                            \
+        void (*dtor)(Deque_##T *);                                             \
+        void (*clear)(Deque_##T *);                                            \
         Deque_##T##_Iterator (*begin)(Deque_##T *);                            \
         Deque_##T##_Iterator (*end)(Deque_##T *);                              \
     };                                                                         \
@@ -112,9 +113,15 @@
         return d->data[i];                                                     \
     }                                                                          \
                                                                                \
-    void Deque_##T##_delete(Deque_##T *d) {                                    \
+    void Deque_##T##_destructor(Deque_##T *d) {                                \
         free(d->data);                                                         \
         free(d);                                                               \
+    }                                                                          \
+                                                                               \
+    void Deque_##T##_clear(Deque_##T *d) {                                     \
+        d->curr_size = 0;                                                      \
+        d->f_idx = -1;                                                         \
+        d->b_idx = 0;                                                          \
     }                                                                          \
                                                                                \
     size_t Deque_##T##_size(Deque_##T *d) {                                    \
@@ -131,22 +138,22 @@
     }                                                                          \
                                                                                \
     void Deque_##T##_Iterator_increment(Deque_##T##_Iterator *it) {            \
-        if(it->curr_idx == it->capacity-1) {                                   \
-            it->data_ptr = it->data_ptr - it->capacity;                        \
+        if(it->curr_idx == it->deque_capacity-1) {                             \
+            it->data_ptr = it->data_ptr - it->deque_capacity;                  \
             it->curr_idx = 0;                                                  \
         } else {                                                               \
-            it->data_ptr = it->data_ptr + 1;                                   \
+            ++it->data_ptr;                                                    \
             ++it->curr_idx;                                                    \
         }                                                                      \
     }                                                                          \
                                                                                \
     void Deque_##T##_Iterator_decrement(Deque_##T##_Iterator *it) {            \
         if(it->curr_idx == 0) {                                                \
-            it->data_ptr = it->data_ptr + it->capacity-1;                      \
-            it->curr_idx = it->capacity-1;                                     \
+            it->data_ptr = it->data_ptr + it->deque_capacity-1;                \
+            it->curr_idx = it->deque_capacity-1;                               \
         } else {                                                               \
-            it->data_ptr = it->data_ptr + 1;                                   \
-            ++it->curr_idx;                                                    \
+            --it->data_ptr;                                                    \
+            --it->curr_idx;                                                    \
         }                                                                      \
     }                                                                          \
                                                                                \
@@ -158,7 +165,7 @@
         Deque_##T##_Iterator it;                                               \
         it.data_ptr = &d->data[d->f_idx];                                      \
         it.curr_idx = d->f_idx;                                                \
-        it.capacity = d->capacity;                                             \
+        it.deque_capacity = d->capacity;                                       \
         it.inc = Deque_##T##_Iterator_increment;                               \
         it.dec = Deque_##T##_Iterator_decrement;                               \
         it.deref = Deque_##T##_Iterator_dereference;                           \
@@ -167,9 +174,10 @@
                                                                                \
     Deque_##T##_Iterator Deque_##T##_end(Deque_##T *d) {                       \
         Deque_##T##_Iterator it;                                               \
-        it.data_ptr = &d->data[d->b_idx];                                      \
-        it.curr_idx = d->b_idx;                                                \
-        it.capacity = d->capacity;                                             \
+        /* Return one past back index */                                       \
+        it.data_ptr = &d->data[d->b_idx+1];                                    \
+        it.curr_idx = d->b_idx+1;                                              \
+        it.deque_capacity = d->capacity;                                       \
         it.inc = Deque_##T##_Iterator_increment;                               \
         it.dec = Deque_##T##_Iterator_decrement;                               \
         it.deref = Deque_##T##_Iterator_dereference;                           \
@@ -184,7 +192,7 @@
     }                                                                          \
                                                                                \
     void Deque_##T##_ctor(Deque_##T *d, bool (*)(const T&, const T&)) {        \
-        d->capacity = 10;                                                      \
+        d->capacity = CAP;                                                      \
         d->curr_size = 0;                                                      \
         d->f_idx = -1;                                                         \
         d->b_idx = 0;                                                          \
@@ -199,7 +207,8 @@
         d->front = &Deque_##T##_front;                                         \
         d->back = &Deque_##T##_back;                                           \
         d->at = &Deque_##T##_at;                                               \
-        d->delet = &Deque_##T##_delete;                                        \
+        d->dtor = &Deque_##T##_destructor;                                     \
+        d->clear = &Deque_##T##_clear;                                         \
         d->size = &Deque_##T##_size;                                           \
         d->empty = &Deque_##T##_empty;                                         \
         d->full = &Deque_##T##_full;                                           \
